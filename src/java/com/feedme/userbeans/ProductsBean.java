e3adc0acc9400a7587336e6742ff9c4aa962cd5c
Giang Nguyen <giangnb@live.com>
2016-12-10 20:47:00 +0700

    User's home page (partial)

--- src/java/com/feedme/userbeans/ProductsBean.java
+++ src/java/com/feedme/userbeans/ProductsBean.java
@@ -7,16 +7,17 @@ package com.feedme.userbeans;
 
 import com.feedme.service.CategoryDTO;
 import com.feedme.service.ProductDTO;
+import com.feedme.ws.Methods;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
 
 /**
  * Managed bean for processing the Category and Products sections
  * @author Giang
  */
 @ManagedBean
-@ViewScoped
+@SessionScoped
 public class ProductsBean {
     private CategoryDTO category; // Selected category
     private String search="";
@@ -33,7 +34,7 @@ public class ProductsBean {
      * @return Category list
      */
     public List<CategoryDTO> doGetCategories() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Methods.fetchCategories();
     }
     
     /**
@@ -52,7 +53,10 @@ public class ProductsBean {
      * @return Back to home page
      */
     public String doSearch(String search) {
-        this.search = search.trim();
+        if (search.matches("\\s")) {
+            search = "";
+        }
+        this.search = search;
         return "index";
     }
     
@@ -76,6 +80,30 @@ public class ProductsBean {
             // No filtering (activated only)
             return getAllProducts();
         }
+    }
+    
+    /**
+     * Reset all filters
+     * @return 
+     */
+    public String doResetFilters() {
+        category = null;
+        search = "";
+        return "index";
+    }
+    
+    public String doGetFilterContext() {
+        String result = "&nbsp;";
+        if (search.length()>0 && category!=null) {
+            result = String.format("Searching for \"%s\" in %s", search, category.getName());
+        }
+        else if (search.length()>0) {
+            result = String.format("Looking for product \"%s\"", search);
+        }
+        else if (category!=null) {
+            result = String.format("Showing category %s", category.getName());
+        }
+        return result;
     }
 
     /**
@@ -109,4 +137,21 @@ public class ProductsBean {
     private List<ProductDTO> getAllProducts() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
+
+    public CategoryDTO getCategory() {
+        return category;
+    }
+
+    public void setCategory(CategoryDTO category) {
+        this.category = category;
+    }
+
+    public String getSearch() {
+        return search;
+    }
+
+    public void setSearch(String search) {
+        this.search = search;
+    }
+    
 }

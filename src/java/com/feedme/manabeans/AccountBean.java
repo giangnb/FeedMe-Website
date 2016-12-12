34f905fc0dce267b0e4f4288a7e04b51d9bc2f5a
Duc Haviet <ducviet.ict@gmail.com>
2016-12-12 21:24:27 +0700

    AccountBean
    - create ManagerLoginBean, ManagerLogoutBean
    - change ManagerPasswordBean
    - check Priviledges

--- src/java/com/feedme/manabeans/AccountBean.java
+++ src/java/com/feedme/manabeans/AccountBean.java
@@ -5,6 +5,9 @@
  */
 package com.feedme.manabeans;
 
+import com.feedme.service.ManagerDTO;
+import com.feedme.service.PriviledgeDTO;
+import com.feedme.ws.Methods;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
@@ -16,10 +19,102 @@ import javax.faces.bean.SessionScoped;
 @SessionScoped
 public class AccountBean {
 
+    private String user;
+    private String pass;
+    private ManagerDTO manager;
+    private PriviledgeDTO priv;
+    private short id;
+
     /**
      * Creates a new instance of AccountBean
      */
     public AccountBean() {
     }
+
+    public String getUser() {
+        return user;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+
+    public ManagerDTO getManager() {
+        return manager;
+    }
+
+    public void setManager(ManagerDTO manager) {
+        this.manager = manager;
+    }
+
+    public PriviledgeDTO getPriv() {
+        return priv;
+    }
+
+    public void setPriv(PriviledgeDTO priv) {
+        this.priv = priv;
+    }
+
+    public short getId() {
+        return id;
+    }
+
+    public void setId(short id) {
+        this.id = id;
+    }
     
+    public String doLoginManager() {
+        manager = Methods.loginManager(user, pass);
+        if (manager == null) {
+            return "page_login";  //Manager login page
+        }
+        return "manager_page";  //Manager page
+    }
+
+    public String doLogoutManager() {
+        manager = null;
+        return "page_login";
+    }
+    
+    public String changePassword() {
+        try {
+            Methods.updateManagerPassword(manager);
+            return "success";  
+        } catch (Exception e) {
+            return "fail";
+        }
+    }
+    
+    public String changeInfo() {
+      return"";
+    }
+
+    public boolean checkIsAdmin() {
+        priv = Methods.fetchPriviledgeById(id);
+        return priv.isAdmin();
+    }
+
+    public boolean checkIsManager() {
+        priv = Methods.fetchPriviledgeById(id);
+        return priv.isManager();
+    }
+
+    public boolean checkIsView() {
+        priv = Methods.fetchPriviledgeById(id);
+        return priv.isView();
+    }
+
+    public boolean checkIsHr() {
+        priv = Methods.fetchPriviledgeById(id);
+        return priv.isHr();
+    }
+    
+    public boolean checkIsEditor() {
+        priv = Methods.fetchPriviledgeById(id);
+        return priv.isEditor();
+    }  
 }
